7b73d549cb249e96b44cb55c7c77833b02843b3f
kabocha <kabocha@not.use>
2020-05-01 00:26:42 +0900

    Added custom keymap

---
+++ keymaps/ver2_0_mouse/keymap.c
@@ -0,0 +1,95 @@
+/* Copyright 2019 Naoto Takai
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+#include "paw3204.h"
+#include "pointing_device.h"
+
+enum layer_names {
+    _BASE,
+    _FN,
+  	_MENU,
+};
+
+#define KC_FN MO(_FN)
+#define KC_MENU MO(_MENU)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT(
+    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_GRAVE, KC_BSPACE,
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET,  KC_BSLASH,
+    KC_LCTRL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_ENTER,
+    KC_LSHIFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT, KC_FN,
+    KC_LGUI,  KC_LALT, KC_SPACE, KC_SPACE, KC_SPACE, KC_RALT, KC_MENU
+  ),
+  [_FN] = LAYOUT(
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,      KC_F9,         KC_F10,   KC_F11,     KC_F12,       _______,   KC_DELETE,
+    _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______, _______, _______,   _______,        _______,  KC_BTN1,    KC_BTN2,  _______,
+    _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_HOME, _______, _______,    _______,       _______,  KC_UP,     KC_ENTER,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,     KC_LEFT,       KC_RIGHT,  KC_FN,
+                                          _______, _______, KC_PGUP, _______, KC_PGDOWN, KC_RALT, KC_DOWN
+  ),
+  [_MENU] = LAYOUT(
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,      KC_F9,         KC_F10,   KC_F11,     KC_F12,       _______,   KC_DELETE,
+    _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______, _______, _______,   _______,        _______,   KC_BTN1,    KC_BTN2,  _______,
+    _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_HOME, _______, _______,    _______,       _______,  _______,     _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,     _______,       _______,  _______,
+                                          _______, _______, KC_WH_U, _______, KC_WH_D, KC_RALT, _______
+  ),
+};
+
+
+report_mouse_t mouse_rep;
+
+void matrix_init_user(void) {
+  init_paw3204();
+}
+
+void matrix_scan_user(void) {
+    static int  cnt;
+    static bool paw_ready;
+    if (cnt++ % 50 == 0) {
+        uint8_t pid = read_pid_paw3204();
+        if (pid == 0x30) {
+            dprint("paw3204 OK\n");
+            paw_ready = true;
+        } else {
+            dprintf("paw3204 NG:%d\n", pid);
+            paw_ready = false;
+        }
+    }
+
+    if (paw_ready) {
+        uint8_t stat;
+        int8_t x, y;
+
+        read_paw3204(&stat, &x, &y);
+        mouse_rep.buttons = 0;
+        mouse_rep.h       = 0;
+        mouse_rep.v       = 0;
+        mouse_rep.x       = y;
+        mouse_rep.y       = -x;
+
+        if (cnt % 10 == 0) {
+            // dprintf("stat:%3d x:%4d y:%4d\n", stat, mouse_rep.x, mouse_rep.y);
+        }
+
+        if (stat & 0x80) {
+            pointing_device_set_report(mouse_rep);
+        }
+    }
+}
---
+++ keymaps/ver2_0_mouse/readme.md
@@ -0,0 +1,5 @@
+# The Custom keymap for choco60 privided kabocha
+
+```
+make choco60_track:ver2_0_mouse
+```
